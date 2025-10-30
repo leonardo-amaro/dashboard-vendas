e5644a811b046af9defa99ab4ca33d9eb1fd1a5d
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-30 21:09:56 +0000

    corrigindo Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 const App = () => {
   return (
-    <>
-      <h1>
-        Dashboard de Vendas
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando com Vite! 🚀
       </h1>
-    </>
+      <p className="mt-4 text-gray-700">Você está pronto para criar interfaces incríveis!</p>
+    </div>
   )
 }
 
